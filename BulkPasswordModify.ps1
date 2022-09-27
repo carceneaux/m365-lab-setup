5405757b368608708dde2927ef073a3a5c6d7214
Chris Arceneaux <carcenea@gmail.com>
2022-09-27 09:33:48 -0400

    initial commit
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

---
+++ BulkPasswordModify.ps1
@@ -0,0 +1,32 @@
+﻿# This Script is used to modify the password of all demo users in the Microsoft Customer Digital Experience (CDX) environment assigning 'Veeam123!' as the PW.
+
+Write-Host "Installing required Azure PowerShell module: AzureAd"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Find-PackageProvider -Name Nuget -ForceBootstrap -IncludeDependencies -Force | Out-Null
+# Determine if AzureAd module is already present
+if ( -not(Get-Module -ListAvailable -Name AzureAd)) {
+    Install-Module -Name AzureAD -SkipPublisherCheck -Force -ErrorAction Stop
+    Write-Host "AzureAD module installed successfully" -ForegroundColor Green
+}
+else {
+    Write-Host "AzureAD module already present" -ForegroundColor Green
+}
+
+# Connecting to Azure
+Connect-AzureAD
+
+# Setting password variable
+$Password = "Veeam123!"
+$SecurePass = $Password | ConvertTo-SecureString -AsPlainText -Force
+
+# Importing user list
+Import-Csv "M365DemoUsers.csv"
+
+# Looping through users
+foreach ($user in $users) {
+    # Set user password
+    Get-AzureAdUser | where-object { $_.DisplayName.SubString(0, 7) -eq 'Student' } | Set-AzureADUserPassword -ObjectID $_.ObjectID -Password $SecurePass
+}
+
+# Set Admin Password
+Get-AzureAdUser | where-object { $_.DisplayName -eq 'MOD Administrator' } | foreach { Set-AzureADUserPassword -ObjectID $_.ObjectID -Password $SecurePass }
---
+++ BulkUserModify.ps1
@@ -0,0 +1,20 @@
+﻿##This will modify all users added to the CSV files listed below
+##Verify the CSV files are in the correct location and named properly.
+
+Write-Host "Installing required Azure PowerShell module: MSOnline"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Find-PackageProvider -Name Nuget -ForceBootstrap -IncludeDependencies -Force | Out-Null
+# Determine if MSOnline module is already present
+if ( -not(Get-Module -ListAvailable -Name MSOnline)) {
+    Install-Module -Name MSOnline -SkipPublisherCheck -Force -ErrorAction Stop
+    Write-Host "MSOnline module installed successfully" -ForegroundColor Green
+}
+else {
+    Write-Host "MSOnline module already present" -ForegroundColor Green
+}
+
+Connect-MsolService
+
+
+Import-Csv "M365DemoUsers.csv" | ForEach-Object { Set-MsolUser -UserPrincipalName $_.UserPrincipalName -DisplayName $_.DisplayName -FirstName $_.FirstName -LastName $_.LastName }
+Import-Csv "M365DemoUsers.csv" | ForEach-Object { Set-MsolUserPrincipalName -UserPrincipalName $_.UserPrincipalName -NewUserPrincipalName $_.NewUserPrincipalName }
