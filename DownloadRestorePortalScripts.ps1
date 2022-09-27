4e9a028d481a67dde4af8ab022eaa0ba1aee1ecc
Chris Arceneaux <carcenea@gmail.com>
2022-09-27 14:28:33 -0400

    adding script
    script downloads restore portal setup scripts
    
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

---
+++ DownloadRestorePortalScripts.ps1
@@ -0,0 +1,15 @@
+# Downloads restore portal scripts
+
+# Change director to Administrator's desktop
+Set-Location -Path "C:\Users\Administrator\Desktop"
+
+# Downloading VeeamHub PowerShell repository
+Invoke-WebRequest "https://codeload.github.com/VeeamHub/powershell/zip/master" -OutFile "scripts.zip"
+
+# Unzipping archive
+Expand-Archive -Path "scripts.zip" -DestinationPath "Restore Portal Scripts"
+Remove-Item "scripts.zip"
+
+# Moving relevant items and deleting the rest
+Move-Item "Restore Portal Scripts\powershell-master\VB365-RestorePortalSetup\*.ps1" "Restore Portal Scripts"
+Remove-Item "Restore Portal Scripts\powershell-master\" -Recurse
